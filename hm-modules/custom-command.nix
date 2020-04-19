74f200d97f77f82c889018df0edc5af6c910b320
Richard Marko <srk@48.io>
2020-04-19 17:26:56 +0200

    cc: better nixpkgs pinning

--- hm-modules/custom-command.nix
+++ hm-modules/custom-command.nix
@@ -142,8 +142,14 @@ in {
         };
         nixos.rebuild = "nixos-rebuild switch";
         nixpkgs = rec {
-          pin = "nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/$1.tar.gz";
-          pinHEAD = "nixpkgs pin $( git rev-parse HEAD )";
+          pin = rec {
+            ref = "nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/$1.tar.gz";
+            commit = ref;
+            head = "nixpkgs pin ref $( git rev-parse HEAD )";
+            # nixpkgs pin rev release-20.03
+            rev = "nixpkgs pin ref $( git rev-parse $1 )";
+            branch = rev;
+          };
           prefetch = pin;
         };
         ca = rec {
