6bd6d72afdadcb0930d18b2ba80c80afea78e1f8
Richard Marko <srk@48.io>
2020-05-08 10:46:09 +0200

    hm/debug: add default

--- hm-modules/debug.nix
+++ hm-modules/debug.nix
@@ -7,10 +7,11 @@
   options = {
     debug.trace = lib.mkOption {
       description = "Call traceSeq on this value";
+      default = 42;
     };
   };
 
   config = {
-    warnings = lib.traceSeq config.debug.trace [];
+    warnings = if config.debug.trace != 42 then lib.traceSeq config.debug.trace [] else [];
   };
 }
