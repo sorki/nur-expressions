25028e4d7b6a4522b06f9969953543851637f8b1
Richard Marko <srk@48.io>
2021-05-02 14:37:17 +0200

    fix ;

--- overlays/src/hnix-store-experiments.nix
+++ overlays/src/hnix-store-experiments.nix
@@ -5,4 +5,4 @@ pkgs.fetchFromGitHub {
   repo = "hnix-store-experiments";
   rev = "de43c4d87011cdf5108bfe48e248cc4d80707ddd";
   sha256 = "14kh1mafja74xjy92i2kfiicb3k1d2qkgsrs6vsby5vw2h9kphzr";
-};
+}
