e1b4d830502759d0af57cb824bfa96daa2b0fa71
Richard Marko <srk@48.io>
2021-05-02 14:54:44 +0200

    actually add src/ircbot.nix

---
+++ overlays/src/ircbot.nix
@@ -0,0 +1,7 @@
+{ pkgs }:
+pkgs.fetchFromGitHub {
+  owner = "stepcut";
+  repo = "ircbot";
+  rev = "cf2a2a092392d93c1b5db8a74d78bb2a5ff3728d";
+  sha256 = "0yzf7da375crvv6fs4npm09bmddx06d4d3g3abh8n98snbqzg2i4";
+}
