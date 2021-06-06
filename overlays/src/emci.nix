5456cd0eeb1d6a9eb7c972cd85401e825d56b429
Richard Marko <srk@48.io>
2021-06-06 12:33:19 +0200

    update emci

--- overlays/src/emci.nix
+++ overlays/src/emci.nix
@@ -2,6 +2,6 @@
 pkgs.fetchFromGitHub {
   owner = "sorki";
   repo = "emci";
-  rev = "dc7c97df5b1e479c2b8e9430e9717db0add5f6ff";
-  sha256 = "1ays7ndxxbpz4641xq656dqyn2jqwlcin0mqgg3fgfinqf40wp4q";
+  rev = "e6129834251d23faca25ddfd583c2ffb04e1b913";
+  sha256 = "1z866jjkynv7fgvm8dj34jnx7pd5w4gskvlvzwh742nl50mskxdf";
 }
