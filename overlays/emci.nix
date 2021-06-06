d810692b7f38419e21cbac4d511fbfaa5899a7bd
Richard Marko <srk@48.io>
2021-06-06 12:32:39 +0200

    move emci source to src/emci.nix

--- overlays/emci.nix
+++ overlays/emci.nix
@@ -1,12 +1,7 @@
 self: super:
 let
   # https://github.com/sorki/emci
-  src = super.fetchFromGitHub {
-    owner = "sorki";
-    repo = "emci";
-    rev = "dc7c97df5b1e479c2b8e9430e9717db0add5f6ff";
-    sha256 = "1ays7ndxxbpz4641xq656dqyn2jqwlcin0mqgg3fgfinqf40wp4q";
-  };
+  src = super.callPackage ./src/emci.nix {};
 
   srcX = ~/git/emci;
 in {
---
+++ overlays/src/emci.nix
@@ -0,0 +1,7 @@
+{ pkgs }:
+pkgs.fetchFromGitHub {
+  owner = "sorki";
+  repo = "emci";
+  rev = "dc7c97df5b1e479c2b8e9430e9717db0add5f6ff";
+  sha256 = "1ays7ndxxbpz4641xq656dqyn2jqwlcin0mqgg3fgfinqf40wp4q";
+}
